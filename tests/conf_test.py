d10122fb643d2cf300cc085f0ac212996712284b
Søren Thomassen <st@randers.dk>
2026-04-16 12:46:13 +0200

    Clean tests

--- tests/conf_test.py
+++ tests/conf_test.py
@@ -21,10 +21,3 @@ def test_healthz(client):
     response = client.get('/healthz')
     assert response.status_code == 200
     assert response.get_json()['status'] == 'success'
-
-
-def test_metrics(client):
-    # POD_NAME env var set in pytest.ini (test-pod)
-    response = client.get('/metrics')
-    assert response.status_code == 200
-    assert 'is_ready gauge\nis_ready{error_type="None",job_name="test-pod"} 1.0' in response.text
